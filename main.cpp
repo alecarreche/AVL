51174014e2451e589469971a83a24a83757dbdc0
Alec Arreche <Alec.arreche@me.com>
2020-09-27 16:41:13 -0400

    setup AVL class and main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <sstream>
+#include <vector>
 using namespace std;
 
 class AVL {
@@ -45,13 +47,54 @@ AVL::AVL(Student* root_) {
 }
 
 int main() {
-    AVL::Student test;
-    test.ID = 0;
-    test.name = "test";
-    test.left = nullptr;
-    test.right = nullptr;
-    test.height = 0;
 
-    AVL test_tree = AVL(&test);
+    cout << "how many commands? "; // remove before submit
+    unsigned long numCommands;
+    cin >> numCommands;
+    cin.ignore(10000, '\n'); // resets cin
+
+    for(unsigned long i = 0; i < numCommands; i++) {
+        string userInput;
+
+        cout << "command: " << i+1 << " "; // remove before submit
+
+        getline(cin, userInput);
+        int spacePos = userInput.find(" ");
+        string command = userInput.substr(0, spacePos);
+
+        cout << command << endl; // remove before submit
+
+        if(command == "insert") {
+
+        } else if(command == "remove") {
+
+        } else if(command == "search") {
+
+        } else if(command == "printInorder") {
+
+        } else if(command == "printPreorder") {
+
+        } else if(command == "printPostorder") {
+
+        } else if(command == "printLevelCount") {
+
+        } else if(command == "removeInorder") {
+
+        }
+
+        string params = userInput.substr(spacePos + 1);
+        int spacePos2 = params.find(" ");
+        string param1 = params.substr(0, spacePos2);
+        cout << userInput << endl;
+        cout << command << endl;
+        cout << param1 << endl;
+    }
+
+
+    /*string user_input;
+    cin >> user_input;
+    cout << user_input;*/
+
+
     return 0;
 }
