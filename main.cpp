e19a8492ecc9c60031a965db58bd560f65d0d191
Alec Arreche <Alec.arreche@me.com>
2020-10-06 16:32:58 -0400

    double rotation fixes

--- main.cpp
+++ main.cpp
@@ -66,15 +66,15 @@ AVL::Student* AVL::insert(Student* node, long ID, string name) {
         node = rotateRight(node);
 
     // right right case
-    else if(node->balance < -1 && node->right->ID)
+    else if(node->balance < -1 && ID > node->right->ID)
         node = rotateLeft(node);
 
     // left right case
-    else if(node->balance > 1 && ID > node->right->ID)
+    else if(node->balance > 1 && ID > node->left->ID)
         node = rotateLeftRight(node);
 
     // right left case
-    else if(node->balance < -1 && ID < node->left->ID)
+    else if(node->balance < -1 && ID < node->right->ID)
         node = rotateRightLeft(node);
 
     return node;
@@ -226,7 +226,6 @@ void AVL::verifyParams(string command, string params) {
         }
 
         string name = params.substr(0, params.size()- 9);
-        //if(name.substr(0, 3) != openQuote || name.substr(name.size()-3, 3) != closeQuote)
         if(name[0] != '\"' || name[name.size()-1] != '\"')
             throw invalid_argument("");
     } else if(command == "remove" || command == "searchID") {
@@ -237,7 +236,6 @@ void AVL::verifyParams(string command, string params) {
                 throw invalid_argument("");
         }
     } else { // searchName case
-        //if(params.substr(0, 3) != openQuote || params.substr(params.size()-3, 3) != closeQuote)
         if(params[0] != '\"' || params[params.size()-1] != '\"')
             throw invalid_argument("");
     }
@@ -271,66 +269,8 @@ AVL::Student* AVL::rotateRightLeft(Student* node) {
 
 int main() {
 
-/*    AVL test;
-    AVL::Student* root = nullptr;
-    root = test.insert(root, 12, "twelve");
-    root = test.insert(root, 8, "eight");
-    root = test.insert(root, 18, "eighteen");
-    root = test.insert(root, 5, "five");
-    root = test.insert(root, 11, "eleven");
-    root = test.insert(root, 17, "seventeen");
-    root = test.insert(root, 4, "four");
-    root = test.insert(root, 7, "seven");
-    root = test.insert(root, 2, "two");
-
-    try {
-        root = test.insert(root, 11, "two");
-    } catch (invalid_argument e){
-        cout << "already in tree" << endl;
-    }
-
-
-    try {
-        AVL::Student *search = test.searchID(root, 1);
-    } catch (invalid_argument e){
-        cout << "not found in tree" << endl;
-    }
-
-    string inorder = test.printInOrder(root);
-    cout << inorder.substr(0, inorder.size() - 2) << endl;
-
-    string postorder = test.printPostOrder(root);
-    cout << postorder.substr(0, postorder.size() - 2) << endl;
-
-    string searchName = test.searchName(root, "seven");
-    cout << searchName.substr(0, searchName.size() - 1) << endl;
-
-    root = test.insert(root, 77, "seven");
-
-    string searchName2 = test.searchName(root, "seven");
-    cout << searchName2.substr(0, searchName2.size() - 1) << endl;
-
-    root = test.removeID(root, 77);
-    string inorder2 = test.printInOrder(root);
-    cout << inorder2.substr(0, inorder2.size() - 2) << endl;
-
-    int removeIdx = 0;
-    test.removeInOrder(root, removeIdx);
-    string inorder3 = test.printInOrder(root);
-    cout << inorder3.substr(0, inorder3.size() - 2) << endl;
-
-
-    vector<AVL::Student*> testVector;
-    testVector = test.inOrderVector(root, testVector);
-    for (auto x : testVector)
-        cout << x->ID << " ";
-
-    cout << "done" << endl;*/
-    string openQuote = "“";
-    string closeQuote = "”";
     AVL tree;
-    AVL::Student *treeRoot = 0;
-    cout << "how many commands? "; // remove before submit
+    AVL::Student *treeRoot = nullptr;
     unsigned long numCommands;
     cin >> numCommands;
     cin.ignore(10000, '\n'); // resets cin
@@ -338,50 +278,46 @@ int main() {
     for(unsigned long i = 0; i < numCommands; i++) {
         string userInput;
 
-        cout << "command: " << i+1 << " "; // remove before submit
-
         getline(cin, userInput);
         int spacePos = userInput.find(" ");
         string command = userInput.substr(0, spacePos);
         string params = userInput.substr(spacePos + 1);
 
-        //cout << command << endl; // remove before submit
-
-        if(command == "insert") {
+        if (command == "insert") {
             try {
                 tree.verifyParams("insert", params);
                 string ID = params.substr(params.size() - 8, 8);
-                string name = params.substr(0, params.size()- 9);
+                string name = params.substr(0, params.size() - 9);
                 long longID = stol(ID);
 //                string nameNoQuote = name.substr(3, name.size()-
-                string nameNoQuote = name.substr(1, name.size()-2);
+                string nameNoQuote = name.substr(1, name.size() - 2);
                 treeRoot = tree.insert(treeRoot, longID, nameNoQuote);
                 cout << "successful" << endl;
-            } catch(invalid_argument e) {
+            } catch (invalid_argument e) {
                 cout << "unsuccessful" << endl;
             }
-        } else if(command == "remove") {
+        } else if (command == "remove") {
             try {
                 tree.verifyParams("remove", params);
                 long longID = stol(params);
                 treeRoot = tree.removeID(treeRoot, longID);
                 cout << "successful" << endl;
-            } catch(invalid_argument e) {
+            } catch (invalid_argument e) {
                 cout << "unsuccessful" << endl;
             }
-        } else if(command == "search") {
+        } else if (command == "search") {
             bool searchID = true;
             long longID;
             try {
                 longID = stol(params);
-            } catch(invalid_argument e) {
+            } catch (invalid_argument e) {
                 searchID = false;
             }
 
-            if(searchID) {
+            if (searchID) {
                 try {
                     tree.verifyParams("searchID", params);
-                    AVL::Student* result = tree.searchID(treeRoot, longID);
+                    AVL::Student *result = tree.searchID(treeRoot, longID);
                     cout << result->name << endl;
                 } catch (invalid_argument e) {
                     cout << "unsuccessful" << endl;
@@ -389,50 +325,38 @@ int main() {
             } else {
                 try {
                     tree.verifyParams("searchName", params);
-                    string nameNoQuote = params.substr(1, params.size()-2);
+                    string nameNoQuote = params.substr(1, params.size() - 2);
                     string result = tree.searchName(treeRoot, nameNoQuote);
-                    if(result == "")
+                    if (result == "")
                         cout << "unsuccessful" << endl;
                     else
                         cout << result.substr(0, result.size() - 1) << endl;
-                } catch(invalid_argument e) {
+                } catch (invalid_argument e) {
                     cout << "unsuccessful" << endl;
                 }
             }
 
-        } else if(command == "printInorder") {
+        } else if (command == "printInorder") {
             string inorder = tree.printInOrder(treeRoot);
             cout << inorder.substr(0, inorder.size() - 2) << endl;
-        } else if(command == "printPreorder") {
+        } else if (command == "printPreorder") {
             string preorder = tree.printPreOrder(treeRoot);
             cout << preorder.substr(0, preorder.size() - 2) << endl;
-        } else if(command == "printPostorder") {
+        } else if (command == "printPostorder") {
             string postorder = tree.printPostOrder(treeRoot);
             cout << postorder.substr(0, postorder.size() - 2) << endl;
-        } else if(command == "printLevelCount") {
+        } else if (command == "printLevelCount") {
             cout << tree.printLevelCount(treeRoot) << endl;
-        } else if(command == "removeInorder") {
+        } else if (command == "removeInorder") {
             try {
                 int N = stoi(params);
                 tree.removeInOrder(treeRoot, N);
-            } catch(invalid_argument e) {
+                cout << "successful" << endl;
+            } catch (invalid_argument e) {
                 cout << "unsuccessful" << endl;
             }
         }
-/*
-        string params = userInput.substr(spacePos + 1);
-        int spacePos2 = params.find(" ");
-        string param1 = params.substr(0, spacePos2);
-        cout << userInput << endl;
-        cout << command << endl;
-        cout << param1 << endl;*/
     }
-
-
-    /*string user_input;
-    cin >> user_input;
-    cout << user_input;*/
-
 
     return 0;
 }
