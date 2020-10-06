7cfca8885bb772afab837b742536d4587cdbedb5
Alec Arreche <Alec.arreche@me.com>
2020-10-06 18:56:54 -0400

    code comments

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <vector>
+
 using namespace std;
 
 class AVL {
@@ -10,8 +11,8 @@ public:
     public:
         long ID;
         string name;
-        Student* left;
-        Student* right;
+        Student *left;
+        Student *right;
         int balance;
 
         Student(long ID_, string name_) {
@@ -24,36 +25,39 @@ public:
     };
 
     // User functions
-    Student* insert(Student* node, long ID, string name);
-    Student* removeID(Student* node, long ID);
-    Student* searchID(Student* node, long ID);
-    string searchName(Student* node, string name);
-    string printInOrder(Student* node);
-    string printPreOrder(Student* node);
-    string printPostOrder(Student* node);
-    int printLevelCount(Student* node);
-    void removeInOrder(Student* node, int N);
+    Student *insert(Student *node, long ID, string name);
+    Student *removeID(Student *node, long ID);
+    Student *searchID(Student *node, long ID);
+    string searchName(Student *node, string name);
+    string printInOrder(Student *node);
+    string printPreOrder(Student *node);
+    string printPostOrder(Student *node);
+    int printLevelCount(Student *node);
+    void removeInOrder(Student *node, int N);
 
     // Helper functions
-    int getHeight(Student* node);
-    Student* findSuccessor(Student* node);
-    vector<Student*> inOrderVector(Student* node, vector<Student*> list);
+    int getHeight(Student *node);
+    Student *findSuccessor(Student *node);
+    vector<Student *> inOrderVector(Student *node, vector<Student *> list);
     void verifyParams(string command, string params);
 
     // Rotations
-    Student* rotateLeft(Student* node);
-    Student* rotateRight(Student* node);
-    Student* rotateLeftRight(Student* node);
-    Student* rotateRightLeft(Student* node);
+    Student *rotateLeft(Student *node);
+    Student *rotateRight(Student *node);
+    Student *rotateLeftRight(Student *node);
+    Student *rotateRightLeft(Student *node);
 
 };
 
-AVL::Student* AVL::insert(Student* node, long ID, string name) {
-    if(!node) {
+// Returns root of tree after inserting a new node containing given ID and
+// name into the tree. Automatically balances tree upon insertion.
+// Throws invalid argument if node with given ID is already in the tree.
+AVL::Student *AVL::insert(Student *node, long ID, string name) {
+    if (!node) {
         return new Student(ID, name);
-    } else if(ID == node->ID) {
+    } else if (ID == node->ID) {
         throw invalid_argument("");
-    } else if(ID < node->ID) {
+    } else if (ID < node->ID) {
         node->left = insert(node->left, ID, name);
     } else {
         node->right = insert(node->right, ID, name);
@@ -62,30 +66,33 @@ AVL::Student* AVL::insert(Student* node, long ID, string name) {
     node->balance = getHeight(node->left) - getHeight(node->right);
 
     // left left case
-    if(node->balance > 1 && ID < node->left->ID)
+    if (node->balance > 1 && ID < node->left->ID)
         node = rotateRight(node);
 
     // right right case
-    else if(node->balance < -1 && ID > node->right->ID)
+    else if (node->balance < -1 && ID > node->right->ID)
         node = rotateLeft(node);
 
     // left right case
-    else if(node->balance > 1 && ID > node->left->ID)
+    else if (node->balance > 1 && ID > node->left->ID)
         node = rotateLeftRight(node);
 
     // right left case
-    else if(node->balance < -1 && ID < node->right->ID)
+    else if (node->balance < -1 && ID < node->right->ID)
         node = rotateRightLeft(node);
 
     return node;
 }
 
-AVL::Student* AVL::removeID(Student* node, long ID) {
-    if(!node)
+// Returns root of tree after deleting the node containing given ID
+// name into the tree. Deleted node is replaced with its inorder successor
+// Throws invalid argument if node with given ID is not in the tree.
+AVL::Student *AVL::removeID(Student *node, long ID) {
+    if (!node)
         throw invalid_argument("");
 
-    if(ID == node->ID) {
-        if(node->right) {
+    if (ID == node->ID) {
+        if (node->right) {
             Student *replacementNode = findSuccessor(node);
             replacementNode->left = node->left;
             replacementNode->right = node->right;
@@ -93,7 +100,7 @@ AVL::Student* AVL::removeID(Student* node, long ID) {
         } else {
             node = node->left;
         }
-    } else if(ID < node->ID)
+    } else if (ID < node->ID)
         node->left = removeID(node->left, ID);
     else
         node->right = removeID(node->right, ID);
@@ -102,22 +109,25 @@ AVL::Student* AVL::removeID(Student* node, long ID) {
 
 }
 
-AVL::Student* AVL::searchID(Student* node, long ID) {
-    if(!node)
+// Returns node with matching ID from a given root node.
+// If ID matches no node in the tree, throws invalid argument.
+AVL::Student *AVL::searchID(Student *node, long ID) {
+    if (!node)
         throw invalid_argument("");
 
-    if(ID == node->ID)
+    if (ID == node->ID)
         return node;
-    else if(ID < node->ID)
+    else if (ID < node->ID)
         return searchID(node->left, ID);
     else
         return searchID(node->right, ID);
 }
 
-string AVL::searchName(Student* node, string name) {
+// Returns space separated string with the ID's of nodes matching given name.
+string AVL::searchName(Student *node, string name) {
     string result = "";
-    if(node) {
-        if(node->name == name)
+    if (node) {
+        if (node->name == name)
             result += to_string(node->ID) + '\n';
 
         result += searchName(node->left, name);
@@ -128,9 +138,11 @@ string AVL::searchName(Student* node, string name) {
 
 }
 
-string AVL::printInOrder(Student* node) {
+// Returns comma separated string containing the names from the inorder
+// traversal of the tree.
+string AVL::printInOrder(Student *node) {
     string result = "";
-    if(node) {
+    if (node) {
         result += printInOrder(node->left);
         result += node->name + ", ";
         result += printInOrder(node->right);
@@ -140,9 +152,11 @@ string AVL::printInOrder(Student* node) {
 
 }
 
-string AVL::printPreOrder(Student* node) {
+// Returns comma separated string containing the names from the preorder
+// traversal of the tree.
+string AVL::printPreOrder(Student *node) {
     string result = "";
-    if(node) {
+    if (node) {
         result += node->name + ", ";
         result += printPreOrder(node->left);
         result += printPreOrder(node->right);
@@ -152,9 +166,11 @@ string AVL::printPreOrder(Student* node) {
 
 }
 
-string AVL::printPostOrder(Student* node) {
+// Returns comma separated string containing the names from the postorder
+// traversal of the tree.
+string AVL::printPostOrder(Student *node) {
     string result = "";
-    if(node) {
+    if (node) {
         result += printPostOrder(node->left);
         result += printPostOrder(node->right);
         result += node->name + ", ";
@@ -163,21 +179,24 @@ string AVL::printPostOrder(Student* node) {
     return result;
 }
 
-int AVL::printLevelCount(Student* node) {
+// Returns number of levels in tree, i.e. height of the root node.
+int AVL::printLevelCount(Student *node) {
     return getHeight(node);
 }
 
-void AVL::removeInOrder(Student* node, int N) {
-    vector<Student*> flattened;
+// Removes the Nth node from the Inorder traversal of the tree.
+void AVL::removeInOrder(Student *node, int N) {
+    vector<Student *> flattened;
     flattened = inOrderVector(node, flattened);
-    if(N >= flattened.size())
+    if (N >= flattened.size())
         throw invalid_argument("");
     else removeID(node, flattened[N]->ID);
 
 }
 
-int AVL::getHeight(Student* node) {
-    if(!node)
+// Returns the height of a given node.
+int AVL::getHeight(Student *node) {
+    if (!node)
         return 0;
 
     int leftHeight = 1;
@@ -189,11 +208,12 @@ int AVL::getHeight(Student* node) {
     return max(leftHeight, rightHeight);
 }
 
-AVL::Student* AVL::findSuccessor(Student* node) {
-    Student* successor = nullptr;
-    if(node->left && node->left->left)
+// Returns the node that is the in order successor of a given node.
+AVL::Student *AVL::findSuccessor(Student *node) {
+    Student *successor = nullptr;
+    if (node->left && node->left->left)
         findSuccessor(node->left);
-    else if(node->right && !node->left) {
+    else if (node->right && !node->left) {
         successor = node->right;
         node->right = successor->right;
     } else {
@@ -204,8 +224,9 @@ AVL::Student* AVL::findSuccessor(Student* node) {
     return successor;
 }
 
-vector<AVL::Student*> AVL::inOrderVector(Student* node, vector<Student*> list) {
-    if(node) {
+// Returns a vector of the inorder traversal of the given tree.
+vector<AVL::Student *> AVL::inOrderVector(Student *node, vector<Student *> list) {
+    if (node) {
         list = inOrderVector(node->left, list);
         list.push_back(node);
         list = inOrderVector(node->right, list);
@@ -214,55 +235,66 @@ vector<AVL::Student*> AVL::inOrderVector(Student* node, vector<Student*> list) {
     return list;
 }
 
+// Verifies that user input is properly formatted. Throws invalid_argument if
+// user input is invalid.
 void AVL::verifyParams(string command, string params) {
     string openQuote = "“";
     string closeQuote = "”";
 
-    if(command == "insert") {
+    if (command == "insert") {
         string ID = params.substr(params.size() - 8, 8);
-        for(auto c : ID) {
-            if(isspace(c))
+
+        for (auto c : ID) {
+            if (isspace(c))
                 throw invalid_argument("");
         }
 
-        string name = params.substr(0, params.size()- 9);
-        if(name[0] != '\"' || name[name.size()-1] != '\"')
+        string name = params.substr(0, params.size() - 9);
+        if (name[0] != '\"' || name[name.size() - 1] != '\"')
             throw invalid_argument("");
-    } else if(command == "remove" || command == "searchID") {
-        if(params.size() != 8)
+    } else if (command == "remove" || command == "searchID") {
+        if (params.size() != 8)
             throw invalid_argument("");
-        for(auto c : params) {
-            if(isspace(c))
+        for (auto c : params) {
+            if (isspace(c))
                 throw invalid_argument("");
         }
     } else { // searchName case
-        if(params[0] != '\"' || params[params.size()-1] != '\"')
+        if (params[0] != '\"' || params[params.size() - 1] != '\"')
             throw invalid_argument("");
     }
 }
 
-AVL::Student* AVL::rotateLeft(Student* node) {
-    Student* grandchild = node->right->left;
-    Student* newParent = node->right;
+// Helper function to perform a left rotation.
+// Derived from Professor Kapoor's slides on AVL Trees
+AVL::Student *AVL::rotateLeft(Student *node) {
+    Student *grandchild = node->right->left;
+    Student *newParent = node->right;
     newParent->left = node;
     node->right = grandchild;
     return newParent;
 }
 
-AVL::Student* AVL::rotateRight(Student* node) {
-    Student* grandchild = node->left->right;
-    Student* newParent = node->left;
+// Helper function to perform a right rotation.
+// Derived from Professor Kapoor's slides on AVL Trees
+AVL::Student *AVL::rotateRight(Student *node) {
+    Student *grandchild = node->left->right;
+    Student *newParent = node->left;
     newParent->right = node;
     node->left = grandchild;
     return newParent;
 }
 
-AVL::Student* AVL::rotateLeftRight(Student* node) {
+// Helper function to perform a left-right rotation.
+// Derived from Professor Kapoor's slides on AVL Trees
+AVL::Student *AVL::rotateLeftRight(Student *node) {
     node->left = rotateLeft(node->left);
     return rotateRight(node);
 }
 
-AVL::Student* AVL::rotateRightLeft(Student* node) {
+// Helper function to perform a right-left rotation.
+// Derived from Professor Kapoor's slides on AVL Trees
+AVL::Student *AVL::rotateRightLeft(Student *node) {
     node->right = rotateRight(node->right);
     return rotateLeft(node);
 }
@@ -275,9 +307,8 @@ int main() {
     cin >> numCommands;
     cin.ignore(10000, '\n'); // resets cin
 
-    for(unsigned long i = 0; i < numCommands; i++) {
+    for (unsigned long i = 0; i < numCommands; i++) {
         string userInput;
-
         getline(cin, userInput);
         int spacePos = userInput.find(" ");
         string command = userInput.substr(0, spacePos);
@@ -289,7 +320,6 @@ int main() {
                 string ID = params.substr(params.size() - 8, 8);
                 string name = params.substr(0, params.size() - 9);
                 long longID = stol(ID);
-//                string nameNoQuote = name.substr(3, name.size()-
                 string nameNoQuote = name.substr(1, name.size() - 2);
                 treeRoot = tree.insert(treeRoot, longID, nameNoQuote);
                 cout << "successful" << endl;
@@ -308,6 +338,7 @@ int main() {
         } else if (command == "search") {
             bool searchID = true;
             long longID;
+
             try {
                 longID = stol(params);
             } catch (invalid_argument e) {
@@ -347,7 +378,9 @@ int main() {
             cout << postorder.substr(0, postorder.size() - 2) << endl;
         } else if (command == "printLevelCount") {
             cout << tree.printLevelCount(treeRoot) << endl;
+
         } else if (command == "removeInorder") {
+
             try {
                 int N = stoi(params);
                 tree.removeInOrder(treeRoot, N);
@@ -355,6 +388,7 @@ int main() {
             } catch (invalid_argument e) {
                 cout << "unsuccessful" << endl;
             }
+
         }
     }
 
